0f1c632053c47a76171c3658d41f4afda9946a85
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2024-06-04 23:10:26 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -9,7 +9,7 @@ public class Email {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
-        //
+
     public String getEmailId() {
         return emailId;
     }
@@ -25,5 +25,33 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        boolean hasEightchar = false;
+        boolean hasUppercase = false;
+        boolean hasLowercase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialchar =false;
+
+        if (newPassword.length() >= 8){
+            hasEightchar = true;
+        }
+
+        for (char c : newPassword.toCharArray()){
+            if (Character.isUpperCase(c)){
+                hasUppercase =true;
+            } else if (Character.isLowerCase(c)) {
+                hasLowercase = true;
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
+            } else {
+                hasSpecialchar = true;
+            }
+        }
+
+        if(oldPassword.equals(getPassword()) && hasEightchar && hasUppercase && hasLowercase && hasDigit && hasSpecialchar){
+            this.password=newPassword;
+            return;
+        }
+        return;
     }
 }
