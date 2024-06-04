1d45d0583052c040632028febdd29815934ad1a3
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2024-06-04 23:12:03 +0530

    Update Workspace.java

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
@@ -12,11 +13,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
+        this.calendar=new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
@@ -25,6 +28,19 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        if(calendar.isEmpty()){
+            return 0;
+        }
+        Collections.sort(calendar, Comparator.comparing(Meeting::getEndTime));
 
+        int maxMeatings = 1;
+        LocalTime lastEndTime=calendar.get(0).getEndTime();
+        for(int i=1;i<calendar.size();i++){
+            if(calendar.get(i).getStartTime().isAfter(lastEndTime)){
+                maxMeatings++;
+                lastEndTime=calendar.get(i).getEndTime();
+            }
+        }
+        return maxMeatings;
     }
 }
